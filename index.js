fd415c6fcf7ac6f575597a4fd44585797b49c2fd
Neel2002 <neel02.shah@gmail.com>
2023-08-29 00:38:43 +0530

    changes views setting in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app= express();
 
 app.use(express.static(__dirname+ "/public"));
-app.set("views" + __dirname + "/views");
+app.set("views" ,"./views");
 
 app.set("view engine", "ejs");
 
